8762c390c4fc0c2a78e940614cf68c799d05bbb1
DARKZONE-MD <darkzonemd@gmail.com>
2025-06-15 08:00:43 +0500

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -13,12 +13,12 @@ cmd({
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
         // Generate system status message
-        const status = `╭━━〔 *✦ 𝙆𝙀𝙀𝙉𝙇𝙔 𝙓𝙈𝘿 ✦* 〕━━┈⊷
+        const status = `╭━━〔 *✦ DARKZONE-MD ✦* 〕━━┈⊷
 ┃🦄╭─────────────·๏
 ┃🦄┃• *⏳Uptime*:  ${runtime(process.uptime())} 
 ┃🦄┃• *📟 Ram usage*: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}GB / ${(os.totalmem() / 1024 / 1024).toFixed(2)}TB
 ┃🦄┃• *⚙️ HostName*: ${os.hostname()}
-┃🦄┃• *👨‍💻 Owner*: ᴏʙᴇᴅᴛᴇᴄʜ
+┃🦄┃• *👨‍💻 Owner*: 𝐸𝑅𝐹𝒜𝒩 𝒜𝐻𝑀𝒜𝒟
 ┃🦄┃• *🧬 Version*: 6.0.0
 ✦ 𝙆𝙀𝙀𝙉𝙇𝙔 𝙓𝙈𝘿 ✦
 ╰──────────────┈⊷
@@ -26,15 +26,15 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 
         // Send the status message with an image
         await conn.sendMessage(from, { 
-            image: { url: `https://files.catbox.moe/9yic1a.jpg` },  
+            image: { url: `https://files.catbox.moe/r2ncqh` },  
             caption: status,
             contextInfo: {
                 mentionedJid: [m.sender],
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363416335506023@newsletter',
-                    newsletterName: '𝙆𝙀𝙀𝙉𝙇𝙔 𝙓𝙈𝘿🎄',
+                    newsletterJid: '120363416743041101@newsletter',
+                    newsletterName: '𝐸𝑅𝐹𝒜𝒩 𝒜𝐻𝑀𝒜𝒟🎄',
                     serverMessageId: 143
                 }
             }
